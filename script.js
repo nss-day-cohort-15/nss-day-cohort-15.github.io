27e1532cda8347e4a3b0861a30f61d8e5487cb27
Matt Kraatz <matt.kraatz@gmail.com>
2016-11-14 20:57:35 -0600

    basic styling

--- script.js
+++ script.js
@@ -35,21 +35,21 @@ let loadPage = () => {
 let populateCards = (infoArr) => {
   for (let i = 0; i < infoArr.length; i++) {
     let memberCard = `
-  <div class="col m4">
-    <h2 class="header">${infoArr[i].name}</h2>
-    <div class="card horizontal">
+  <div class="col s6 m4 l3">
+    <div class="card">
       <div class="card-image memCard">
         <img src="https://unsplash.it/150/?random">
+        <span class="card-title">${infoArr[i].name}</span>
       </div>
-      <div class="card-stacked">
         <div class="card-content">
           <p>${infoArr[i].aboutMe}</p>
         </div>
         <div class="card-action">
-           <a href="${infoArr[i].githubLink}">GitHub</a>
-              <a href="${infoArr[i].portfolioLink}">Portfolio</a>
-              <a href="${infoArr[i].linkedInLink}">LinkedIn</a>
-        </div>
+          <div class="row">
+            <a href="${infoArr[i].githubLink}"><img src="img/icon/github.png"></a>
+            <a href="${infoArr[i].linkedInLink}"><img src="img/icon/linkedin.png"></a>
+            <a href="${infoArr[i].portfolioLink}"><i class="material-icons">open_in_new</i></a>
+          </div>
         </div>
   </div>`
     $('.members-area-box').append(memberCard)
