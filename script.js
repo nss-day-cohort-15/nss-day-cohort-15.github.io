7b18a9ee066a66d1b6bf8cf2a44d0adbc0e2b31a
Matt Hamil <j.matthew.hamil@gmail.com>
2016-12-07 20:31:58 -0600

    Added mouse-over animation for tiles

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
-// Simple ajax call to get class info array from json file
+$(document).ready(function () {
 
-function getClass() {
-  return new Promise((resolve, reject) => {
-        $.ajax({
-          url: "classinfo.json",
-        }).done(function(response) {
-        console.log(response, "response")
-          resolve(response)
-        }).fail(function(error) {
-          reject(error)
-        })
+  // Click-to-scroll for animated arrow
+  $('#about-us').click(function () {
+      $('html,body').animate({
+          scrollTop: $(".meet-us--tiles").offset().top},
+          'slow');
+  });
+
+  // XHR to load class info from json file
+  function getClassFromJson() {
+    return new Promise(function (resolve, reject) {
+      $.ajax({
+        url: "classinfo.json",
+      }).done(function(response) {
+        resolve(response);
+      }).fail(function(error) {
+        reject(error);
       })
-}
+    });
+  }
 
-// On page load, send ajax call, extract the array from containing object and inject each item into Materialize card
-function loadPage() {
-  var infoArr = [];
-  getClass()
-  .then((response) => {
-    for (var key in response) {
-      infoArr = response[key]
-    }
-    var cardString = generateCards(infoArr);
-    attachCardsToDOM(cardString);
+  // Load the class info, load the DOM with cards
+  function loadPage() {
+    getClassFromJson()
+    .then(function (response) {
+      var propertyName = Object.keys(response)[0];
+      var infoArr = response[propertyName];
+      var cardString = generateCards(infoArr);
 
-    //Add some sort of functionality on hover
-    $(".memCard").hover(function(e) {
-      console.log('this can swap the card to personality shot, produce a modal, or do whatever else we want')
-    })
-  })
-}
+      attachCardsToDOM(cardString);
+    });
+  }
 
-function generatePlaceholderAvatar(index) {
-  return "https://api.adorable.io/avatars/200/nss-cohort-15" + index;
-}
+  function generatePlaceholderAvatar(index) {
+    return "https://api.adorable.io/avatars/200/nss-cohort-15" + index;
+  }
 
-function generateCards(peopleArr) {
-  return peopleArr.reduce(function(domString, person, i) {
-    domString += "<div class='col-xs-12 col-md-4 col-lg-3 person-tile'>" +
-                 "<img src='" + /* Replace this with professional pic --> */ generatePlaceholderAvatar(i) +
-                    "' alt='" + person.name + "''>" +
-                 "<h3>" + person.name.split(" ")[0] + "<br/>" + person.name.split(" ")[1] + "</h3>" +
-                 "<hr>" +
-                 "<div class='tile--icons'>" +
-                     "<a href='" + person.githubLink + "' target='_blank'>" +
-                         "<img src='img/logo/Github.svg' alt='Github'/>" +
-                     "</a>" +
-                     "<a href='" + person.linkedInLink + "' target='_blank'>" +
-                         "<img src='img/icon/linkedin.svg' alt='Linkedin'/>" +
-                     "</a>" +
-                     "<a href='" + person.portfolioLink + "' target='_blank'>" +
-                         "<img src='img/icon/globe.svg' alt='Personal Website'/>" +
-                     "</a>" +
+  function generateCards(peopleArr) {
+    return peopleArr.reduce(function(domString, person, i) {
+      domString += "<div class='col-xs-12 col-md-4 col-lg-3 person-tile'>" +
+                   "<div class='image-container'>" +
+                      "<img class='person-image' src='" + /* Replace this with professional pic --> */ generatePlaceholderAvatar(i) +
+                        "' alt='" + person.name + "''>" +
+                      "<div class='person-bio'><span>" + person.aboutMe + "</span></div>" +
                    "</div>" +
-                 "</div>";
+                   "<h3>" + person.name.split(" ")[0] + "<br/>" + person.name.split(" ")[1] + "</h3>" +
+                   "<hr>" +
+                   "<div class='tile--icons'>" +
+                       "<a href='" + person.githubLink + "' target='_blank'>" +
+                           "<img src='img/logo/Github.svg' alt='Github'/>" +
+                       "</a>" +
+                       "<a href='" + person.linkedInLink + "' target='_blank'>" +
+                           "<img src='img/icon/linkedin.svg' alt='Linkedin'/>" +
+                       "</a>" +
+                       "<a href='" + person.portfolioLink + "' target='_blank'>" +
+                           "<img src='img/icon/globe.svg' alt='Personal Website'/>" +
+                       "</a>" +
+                     "</div>" +
+                   "</div>";
 
-    return domString;
-  }, '');
-}
+      return domString;
+    }, '');
+  }
 
-function attachCardsToDOM(cardString) {
-  document.getElementById('meet-us--row').innerHTML = cardString;
-}
-loadPage()
+  function attachCardsToDOM(cardString) {
+    document.getElementById('meet-us--row').innerHTML = cardString;
+  }
+
+  loadPage();
+});
