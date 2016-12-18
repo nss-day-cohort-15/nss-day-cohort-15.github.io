c8a6e83a7f6398dc2b59d9476c2b9807637c9231
Matt Kraatz <matt.kraatz@gmail.com>
2016-12-18 12:25:16 -0600

    added individual and class photos

--- script.js
+++ script.js
@@ -32,15 +32,11 @@ $(document).ready(function () {
     });
   }
 
-  function generatePlaceholderAvatar(index) {
-    return "https://api.adorable.io/avatars/200/nss-cohort-15" + index;
-  }
-
   function generateCards(peopleArr) {
     return peopleArr.reduce(function(domString, person, i) {
       domString += "<div class='col-sm-6 col-md-4 col-lg-3 person-tile'>" +
                    "<div class='image-container'>" +
-                      "<img class='person-image' src='" + /* Replace this with professional pic --> */ generatePlaceholderAvatar(i) +
+                      "<img class='person-image' src='" + person.professionalPic +
                         "' alt='" + person.name + "''>" +
                       "<div class='person-bio'><span>" + person.aboutMe + "</span></div>" +
                    "</div>" +
